7d3b14c40dca0cdc7b2355c1ae7acc1b0fbf3d84
CoderGrammer <codergrammer@gmail.com>
2021-09-14 04:46:41 +0100

    Add junit 5 and 4 setup

---
+++ junit-5-maven/src/test/java/JUnit4Test.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class JUnit4Test {
+
+    @Test
+    public void aTest() {
+        Assert.fail();
+    }
+
+}
